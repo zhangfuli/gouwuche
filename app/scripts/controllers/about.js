69f8893978f7fe0f0d97d910f2d18377788dd2b7
zhangfuli <1171067930@qq.com>
2016-09-02 18:54:15 +0800

    angular 购物车

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gouwucheApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gouwucheApp
+ */
+angular.module('gouwucheApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,104 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gouwucheApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gouwucheApp
+ */
+angular.module('gouwucheApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  })
+  .controller('firstController', function($scope,$http) {
+  //$http.get().then(function(data){
+  //  $scope.Product = data;
+  //});
+  $scope.Product = [{
+    id: 1,
+    name: "零食1",
+    quantity: 1,
+    price: 1
+  }, {
+    id: 2,
+    name: "零食2",
+    quantity: 1,
+    price: 8
+
+  }, {
+    id: 3,
+    name: "零食3",
+    quantity: 3,
+    price: 4
+  }, {
+    id: 4,
+    name: "零食4",
+    quantity: 1,
+    price: 3
+  }];
+
+  $scope.totalPrice = function() {
+    var total = 0;
+    angular.forEach($scope.Product, function(item) {
+      total += item.quantity * item.price;
+    });
+    return total;
+  };
+
+  $scope.totalQuantity = function() {
+    var total = 0;
+    angular.forEach($scope.Product, function(item) {
+      total += parseInt(item.quantity);
+    });
+    return total;
+  };
+
+  $scope.remove = function(index) {
+    $scope.Product.splice(index, 1);
+  };
+
+  $scope.removeall = function() {
+    var index;
+    for (index = $scope.Product.length - 1; index >= 0; index--) {
+      $scope.remove(index);
+    }
+  };
+
+  $scope.reduce = function(index) {
+    if ($scope.Product[index].quantity != 1) {
+      $scope.Product[index].quantity--;
+    } else {
+      var ans = confirm("是否移除该产品？");
+      if (ans) {
+        $scope.remove(index);
+      } else {
+        $scope.Product[index].quantity = 1;
+      }
+    }
+  };
+
+  $scope.add = function(index) {
+    $scope.Product[index].quantity++;
+  };
+
+  $scope.$watch('Product', function(newValue, oldValue) {
+    angular.forEach(newValue, function(item, key) {
+      if (item.quantity < 1) {
+        var ans = confirm("是否移除该产品？");
+        if (ans) {
+          $scope.remove(key);
+        } else {
+          item.quantity = oldValue[key].quantity;
+        }
+        return;
+      }
+    });
+  }, true);
+
+});
+;
